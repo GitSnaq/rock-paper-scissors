2182bd809bcfa008be55febdf9b7a656308aa790
GitSnaq <saqfire@gmail.com>
2022-05-10 17:53:33 +0200

    Forgot to commit while working on a bunch of stuff. Basically added UI with a button that updates a result with who won

--- script.js
+++ script.js
@@ -27,45 +27,81 @@ function roundOfGameplay(playerSelection, computerSelection){
         return "was a draw"
     }
 }
+
 // console.log(roundOfGameplay("Rock", computerPlay()))
-console.log(game())
+// console.log(game())
+const results = document.querySelector(".display-result");
 
-function game(){
-    playerCount = 0;
-    computerCount = 0;
-    i = 0;
-    while (i<10){
-        let input = prompt("Enter rock, paper, scissor")
-        pcChoice = computerPlay();
-        result = roundOfGameplay(input, pcChoice)
-        console.log(result)
-        i++
-        if (result==="player wins"){
-            playerCount++
-        }
-        else if(result==="computer wins"){
-            computerCount++
-        }
-    }
-    finalResult = [playerCount, computerCount]
-    if (playerCount>computerCount){
-        playerCount = Number(playerCount)
-        computerCount = Number(computerCount)
-        return `final score was:(${finalResult}) where the Player won ${playerCount-computerCount} more games`
-    }
-    else if(playerCount<computerCount){
-        playerCount = Number(playerCount)
-        computerCount = Number(computerCount)
-        return `final score was:(${finalResult}) where the Computer won ${computerCount-playerCount} more games`
-    }
-    else{
-        return "It was a draw"
-    }
-}
+const rock = document.querySelector(".rock");
+rock.addEventListener("click", ()=>{
+    results.textContent = roundOfGameplay("rock", computerPlay())})
 
-//currently the code only asks user once for input and then uses that all rounds
-//need to make the program wait for input for all rounds
-//and maybe let user select number of rounds to be played
+const paper = document.querySelector(".paper");
+paper.addEventListener("click", ()=>{
+    results.textContent = roundOfGameplay("paper", computerPlay())})
+
+const scissor = document.querySelector(".scissor");
+scissor.addEventListener("click", ()=>{
+    results.textContent = roundOfGameplay("scissor", computerPlay())})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function game(){
+//     playerCount = 0;
+//     computerCount = 0;
+//     i = 0;
+//     while (i<10){
+//         let input = prompt("Enter rock, paper, scissor")
+//         pcChoice = computerPlay();
+//         result = roundOfGameplay(input, pcChoice)
+//         console.log(result)
+//         i++
+//         if (result==="player wins"){
+//             playerCount++
+//         }
+//         else if(result==="computer wins"){
+//             computerCount++
+//         }
+//     }
+//     finalResult = [playerCount, computerCount]
+//     if (playerCount>computerCount){
+//         playerCount = Number(playerCount)
+//         computerCount = Number(computerCount)
+//         return `final score was:(${finalResult}) where the Player won ${playerCount-computerCount} more games`
+//     }
+//     else if(playerCount<computerCount){
+//         playerCount = Number(playerCount)
+//         computerCount = Number(computerCount)
+//         return `final score was:(${finalResult}) where the Computer won ${computerCount-playerCount} more games`
+//     }
+//     else{
+//         return "It was a draw"
+//     }
+// }
 
 
 
